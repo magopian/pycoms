9c71be21b31f26b697d14d2064d1138ae152c72a
Mathieu Agopian <mathieu@trustive.com>
2011-10-08 20:47:46 +0200

    cleaned version of the notmyidea-cms template (css streamline, templates improvement)

--- settings.py
+++ settings.py
@@ -8,7 +8,13 @@ DEFAULT_LANG = 'en'
 DEFAULT_DATE_FORMAT = '%Y-%m-%d'
 
 SOCIAL = (
-    ('twitter', 'http://twitter.com/magopian'),
+    ('@thepsf', 'http://twitter.com/thepsf'),
+    ('@pycon', 'http://twitter.com/pycon'),
+)
+LINKS = (
+    ('Python', 'http://python.org'),
+    ('PSF', 'http://www.python.org/psf/'),
+    ('PyCon', 'http://pycon.org'),
 )
 
 REVERSE_ARCHIVE_ORDER = True
